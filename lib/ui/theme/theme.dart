6891cf2b3aa2fc65e0c3e144d926e797d7e84e3b
Aleksey Yakimov <lexuschert@gmail.com>
2024-03-04 17:58:02 +0400

    init theme data for light and dark

---
+++ lib/ui/theme/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color.fromRGBO(0, 207, 170, 1);
+
+final darkData = ThemeData(
+  useMaterial3: true,
+  primaryColor: primaryColor,
+);
+
+final lightData = ThemeData(
+  useMaterial3: true,
+  primaryColor: primaryColor,
+);
